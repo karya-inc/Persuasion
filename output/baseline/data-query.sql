1a5b484dc3b88303dbbf6ffda69de5a5bf5b7e41
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-02-18 10:45:26 +0530

    add wave1 randomization list

--- output/baseline/data-query.sql
+++ output/baseline/data-query.sql
@@ -1,6 +1,7 @@
 SELECT 
     worker_id,
-    -- Geo-Fetched Address --
+    phone_number,
+
     address->>'locality' AS address_locality,
     address->>'premises' AS address_premises,
     address->>'adminArea' AS address_admin_area,
@@ -11,7 +12,6 @@ SELECT
     address->>'subAdminArea' AS address_sub_admin_area,
     address->>'thoroughfare' AS address_thoroughfare,
     
-    -- worker profile -- 
     profile->>'yob' AS profile_year_of_birth,
     profile->>'gender' AS profile_gender,
     profile->>'pincode' AS profile_pincode,
@@ -26,7 +26,6 @@ SELECT
     profile->>'employment_status' AS profile_employment_status,
     profile->>'languages_selected' AS profile_languages_selected,
 
-    -- baseline: responsent's background --
     baseline->'10677'->>'name' AS baseline_name,
     baseline->'10677'->>'last_name' AS baseline_last_name,
     baseline->'10677'->>'sleep_at' AS baseline_sleep_at,
@@ -43,27 +42,25 @@ SELECT
     baseline->'10677'->>'birthplace_state' AS baseline_birthplace_state,
     baseline->'10677'->>'birthplace_district' AS baseline_birthplace_district,
 
-    -- social media belief --
     baseline->'10678'->>'social_media_impact' AS baseline_social_media_impact,
     baseline->'10678'->>'social_media_frequency' AS baseline_social_media_frequency,
 
-    -- women's agency answers for wife --
     baseline->'10678'->>'consultation' AS baseline_consultation,
     baseline->'10678'->>'alone_friends' AS baseline_alone_friends,
     baseline->'10678'->>'market_permission' AS baseline_market_permission,
     baseline->'10678'->>'public_transportation' AS baseline_public_transportation,
     baseline->'10678'->>'clothing_purchase_permission' AS baseline_clothing_purchase_permission,
     baseline->'10678'->>'expensive_purchase' AS baseline_expensive_purchase,
+    baseline->'10678'->>'friends_neighborhood' AS baseline_friends_neighborhood,
 
-    -- women's agency answers for mother --
     baseline->'10678'->>'consultation_m' AS baseline_consultation_m,
     baseline->'10678'->>'alone_friends_m' AS baseline_alone_friends_m,
     baseline->'10678'->>'market_permission_m' AS baseline_market_permission_m,
     baseline->'10678'->>'public_transportation_m' AS baseline_public_transportation_m,
     baseline->'10678'->>'clothing_purchase_permission_m' AS baseline_clothing_purchase_permission_m,
     baseline->'10678'->>'expensive_purchase_m' AS baseline_expensive_purchase_m,
+    baseline->'10678'->>'friends_neighborhood_m' AS baseline_friends_neighborhood_m,
 
-    -- baseline survey - other beliefs --
     baseline->'10679'->>'statement_one' AS baseline_husband_failed_responsibility,
     baseline->'10679'->>'income_inequality' AS baseline_income_inequality,
     baseline->'10679'->>'statement_two_input' AS baseline_wife_failed_responsibility,
@@ -73,6 +70,7 @@ FROM (
         baseline_data.worker_id,
         w.profile->'address' AS address,
         w.profile->'data' AS profile,
+        w.phone_number AS phone_number,
         jsonb_object_agg(baseline_data.task_id, baseline_data.data) AS baseline
     FROM (
         SELECT 
@@ -88,5 +86,5 @@ FROM (
     JOIN 
         worker w ON baseline_data.worker_id = w.id
     GROUP BY 
-        baseline_data.worker_id, w.profile->'address', w.profile->'data'
+        baseline_data.worker_id, w.profile->'address', w.profile->'data', w.phone_number
 ) subquery;
