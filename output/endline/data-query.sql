223675b91320d00c9287976cd17a07dfd2fb17f6
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-03-16 15:46:06 +0530

    add original file name in subjective eval report

--- output/endline/data-query.sql
+++ output/endline/data-query.sql
@@ -3,11 +3,14 @@ SELECT
     mta.output->'data'->>'sound_legible' as sound_legible,
     mta.output->'data'->>'recording_confidence' as recording_confidence,
     mta.output->'data'->>'eligible_for_training' as eligible_for_training,
-    mt.input->'files'->>'audio' as audio
+    mt.input->'files'->>'audio' as audio,
+    kf.orig_name as original_name
 FROM 
     microtask_assignment mta
 JOIN
-    microtask mt on mta.microtask_id = mt.id 
+    microtask mt on mta.microtask_id = mt.id
+JOIN 
+    karya_file kf on mt.input->'files'->>'audio' = kf.name
 WHERE
     mta.task_id = '12266' AND mta.status = 'VERIFIED'
 ORDER BY
